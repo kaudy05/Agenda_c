bbe831b66a669931a2c41dfeb64cd08397cfc10e
kaudy05 <kauysantosferreira109@gmail.com>
2024-09-13 19:26:21 -0300

    Update listaEstatica.h

--- listaEstatica.h
+++ listaEstatica.h
@@ -2,14 +2,12 @@
 #define MAX_CONTATOS 100
 
 
-// Estrutura para armazenar informações de um contato
 typedef struct {
     char nome[100];
     char telefone[15];
     char email[50];
 } Contato;
 
-// Funções da agenda
 
 void adicionarContato(Contato agenda[], int *contador);
 void listarContatos(Contato agenda[], int contador);
